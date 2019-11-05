bb988b08cd04d990c986e1758c9b4115b358ff56
kurchenko <kurchenko@A24PC4>
2019-11-05 17:38:57 +0200

    Update EFContext

--- MedicalAssistant/MedicalAssistant/Startup.cs
+++ MedicalAssistant/MedicalAssistant/Startup.cs
@@ -5,6 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using Microsoft.AspNetCore.Identity;
+using System.Text;
 
 namespace MedicalAssistant
 {
@@ -20,41 +25,41 @@ namespace MedicalAssistant
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<EFDbContext>(options =>
-               options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<EFDbContext>(options =>
+            //   options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<DbUser, DbRole>(options => options.Stores.MaxLengthForKeys = 128)
-                .AddEntityFrameworkStores<EFDbContext>()
-                .AddDefaultTokenProviders();
+            //services.AddIdentity<DbUser, DbRole>(options => options.Stores.MaxLengthForKeys = 128)
+            //    .AddEntityFrameworkStores<EFDbContext>()
+            //    .AddDefaultTokenProviders();
 
-            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("11-sdfasdf-22233222222"));
+            //var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("11-sdfasdf-22233222222"));
 
-            services.Configure<IdentityOptions>(options =>
-            {
-                // Default Password settings.
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequiredUniqueChars = 0;
-            });
+            //services.Configure<IdentityOptions>(options =>
+            //{
+            //    // Default Password settings.
+            //    options.Password.RequireNonAlphanumeric = false;
+            //    options.Password.RequiredUniqueChars = 0;
+            //});
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(cfg =>
-            {
-                cfg.RequireHttpsMetadata = false;
-                cfg.SaveToken = true;
-                cfg.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    IssuerSigningKey = signingKey,
-                    ValidateAudience = false,
-                    ValidateIssuer = false,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    // set ClockSkew is zero
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(cfg =>
+            //{
+            //    cfg.RequireHttpsMetadata = false;
+            //    cfg.SaveToken = true;
+            //    cfg.TokenValidationParameters = new TokenValidationParameters()
+            //    {
+            //        IssuerSigningKey = signingKey,
+            //        ValidateAudience = false,
+            //        ValidateIssuer = false,
+            //        ValidateLifetime = true,
+            //        ValidateIssuerSigningKey = true,
+            //        // set ClockSkew is zero
+            //        ClockSkew = TimeSpan.Zero
+            //    };
+            //});
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the React files will be served from this directory
@@ -67,7 +72,7 @@ namespace MedicalAssistant
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            dbContext.Database.EnsureCreated();
+            //dbContext.Database.EnsureCreated();
             
             if (env.IsDevelopment())
             {
