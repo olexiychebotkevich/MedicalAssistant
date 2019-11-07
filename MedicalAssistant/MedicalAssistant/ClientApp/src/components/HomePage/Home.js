1fc061d73a5dba410315281f84b316d3c5423ef0
Elizaveta Shemetovska <shemetovska1107@gmail.com>
2019-11-08 00:34:49 +0200

    Add background-image , edit HomePage and RegisterPage

--- MedicalAssistant/MedicalAssistant/ClientApp/src/components/HomePage/Home.js
+++ MedicalAssistant/MedicalAssistant/ClientApp/src/components/HomePage/Home.js
@@ -5,11 +5,12 @@ import '../home.css';
 import {
     Form,
     Input,
-   Checkbox,
     Icon,
-
+    Typography ,
     Button,
 } from 'antd';
+
+
 
 
 
@@ -24,57 +25,71 @@ class NormalLoginForm extends React.Component {
   };
 
   render() {
-   
+
   
+const { Title } = Typography;
     const { getFieldDecorator } = this.props.form;
     return (
-      <Form  onSubmit={this.handleSubmit} className="login-form">
-        <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })}
-        <a className="login-form-forgot" href="">
+      
+//  <h1>
+//   Medical Assistant
+//  </h1>
+    
+       
+<div className="container-fluid">
+ 
+<h1 className="header">Medical Assistant</h1>
+  
+
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="Username"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                />,
+              )}
+              <a className="login-form-forgot" href="">
             Forgot password
           </a>
-          </Form.Item>
-          <Form.Item >
-            <div className="row">
+            </Form.Item>
+            
+              <Form.Item>
+              <div className="row">
               <div className="col-6">
-          <Button type="dashed" htmlType="submit" className="login-form-button">
-            Log in
+             
+        <Button type="dashed" htmlType="submit" className="login-form-button" >
+             Log in
           </Button>
-          </div>
+           </div>
           <div className="col-6">
-          <Button type="dashed" htmlType="submit"  className="register-form-button">
+          <Button type="dashed" htmlType="submit"  className="register-form-button" >
           Register
           </Button>
-          </div>
+           </div>
           </div>
         </Form.Item>
+          </Form>
+        
        
-      </Form>
+ 
+ 
+  </div>
+        
+         
     );
   }
 }
