7c170a52a45fc02883c63233f074a38cf0e5ba83
Elizaveta Shemetovska <shemetovska1107@gmail.com>
2020-03-15 13:27:01 +0200

    tmp

--- MedicalAssistant/MedicalAssistant/ClientApp/src/components/More_Recipe/index.js
+++ MedicalAssistant/MedicalAssistant/ClientApp/src/components/More_Recipe/index.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 import get from 'lodash.get';
 import { connect } from "react-redux";
 import SpinnerWidget from '../spinner';
-
+import './index.css';
 import {
   Row, Card, Col
 } from 'antd';
@@ -40,7 +40,7 @@ class NormalMoreForm extends React.Component {
           {this.state.recipe ?
             recipe.tablets.map((pill, index) =>
               <Col xs={25} sm={25} md={12} lg={8} xl={8}>
-                <Card key={index} className="cards" title="Name Pill" extra={pill.name} headStyle={{ color: ' rgb(221, 252, 200)', fontFamily: 'Candara' }} style={{ backgroundColor: 'rgb(157,181,167)', fontFamily: 'Candara', fontWeight: '500', marginTop: "10px" }} >
+                <Card key={index} className="cards"  title={pill.name} headStyle={{ color: ' rgb(221, 252, 200)', fontFamily: 'Candara' }} style={{ backgroundColor: 'rgb(157,181,167)', fontFamily: 'Candara', fontWeight: '500', marginTop: "10px" }} >
                   <p className="pstyle" >Кількість днів: {pill.countdays}</p>
                   <p className="pstyle">Особливості прийому: {pill.receptionfeatures}</p>
                   <p className="pstyle">Кількість разів в день: {pill.counttimesaday}</p>
